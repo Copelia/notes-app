bcdab23dd0bc916e4b33b123ccbdf925696d9108
Coral <iamcorallium@gmail.com>
2020-01-28 15:49:54 -0600

    Removing notes

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 // const fs = require('fs');
 const chalklib = require('chalk');
-const getNotes = require('./notes.js');
 const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const myFunction = getNotes();
-console.log(myFunction);
-const mymessage = chalklib.red.underline.inverse('You win!');
-console.log(mymessage);
+// Customize yargs version
+yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Adds a new note',
-    handler: function () {
-        console.log('Adding note');
+    builder: {
+      title: {
+          describe: 'Note title',
+          // Require argument or not, it's false for default
+          demandOption: true,
+          type: 'string'
+      },
+      body: {
+          describe: 'Note content',
+          demandOption: true,
+          type: 'string'
+      }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -22,8 +33,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Removes a note',
-    handler: function () {
-        console.log('Removing note');
+    builder: {
+      title: {
+          describe: 'Note title',
+          demandOption: true,
+          type: 'string'
+      }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
 
@@ -48,8 +66,4 @@ yargs.command({
 })
 
 
-console.log(yargs.argv);
-
-
-// fs.writeFileSync('mynotes.txt', 'Step two');
-// fs.appendFileSync('mynotes.txt', 'Step one and two');
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,5 +1,59 @@
-const getNotes = () => {
-    return 'Your notes...'
+const fs = require('fs');
+
+
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title;
+    });
+
+    if(duplicateNotes.length === 0){   
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log('New note added');
+    } else {
+        console.log("Note title taken, try again plz")
+    }
+
+};
+
+const removeNote = function (title) {
+    const notes = loadNotes();
+
+    const notesToKeep = notes.filter(function(note) {
+       return note.title !== title 
+    });
+
+    saveNotes(notesToKeep);
+
 }
 
-module.exports = getNotes
+// We stringify the object for a json file to be able to save it
+const saveNotes = function (notes) {
+    const JSONdata = JSON.stringify(notes);
+    // Then we sabe it in the json file
+    fs.writeFileSync('notes.json', JSONdata)
+}
+
+const loadNotes = function () {
+    try{
+        const data = fs.readFileSync('notes.json');
+        const JSONdata = data.toString();
+         return JSON.parse(JSONdata);
+
+    } 
+    // If there is no file yet, then return an array where you start to push an object
+    catch (e){
+        return [];
+
+    }
+}
+
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote
+
+}
